aaae100f9425aa17013f18a8553a0eb0b07f33ec
WasabiRaptor <ketchupraptor@gmail.com>
2021-10-03 20:18:09 -0400

    starting work at status based animation

---
+++ stats/speciesAnimOverride/speciesAnimOverride.lua
@@ -0,0 +1,41 @@
+function init()
+	self.species = world.entitySpecies(entity.id())
+	self.animationData = root.assetJson("/stats/speciesAnimOverride/"..config.getParameter("animationConfig")).animatedParts.stateTypes
+	self.animStateData = root.assetJson("/humanoid/"..self.species.."/speciesAnimOverride.config")
+
+	for partname, filepath in pairs(self.animStateData.partImages) do
+		animator.setPartTag(partname, "partImage", filepath)
+	end
+
+end
+
+function update(dt)
+	animator.setFlipped(mcontroller.facingDirection() == -1)
+	if mcontroller.walking() then
+
+	end
+	if mcontroller.running() then
+
+	end
+	if mcontroller.falling() then
+
+	end
+	if mcontroller.jumping() then
+
+	end
+	if mcontroller.crouching() then
+
+	end
+	if mcontroller.flying() then
+
+	end
+	if mcontroller.groundMovement() then
+
+	end
+	if mcontroller.liquidMovement() then
+
+	end
+end
+
+function uninit()
+end
